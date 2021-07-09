8db43d6ae58ec868f32d30daac78e9801f6eb82c
kelvia sousa <kelviaks@gmail.com>
2021-07-09 15:08:26 -0300

    deply

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,7 @@ import { musicRouter } from "./routes/musicRouter";
 import { userRouter } from "./routes/userRouter";
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
